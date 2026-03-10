7a119b827d8096f6ab03b1c9bacfeab80d8ce925
James1oliveira <oliveira1james@gmail.com>
2026-03-10 10:23:57 +0200

    'update'

---
+++ book 2/book_store/app/about/page.jsx
@@ -0,0 +1,15 @@
+
+"use client";
+import { useState } from "react";
+
+export const metadata = {
+  title: "About Greg's Portfolio of Small Bets",
+  description: "Tech Courses and Books",
+  keywords: "passive income, small bets, tech courses, tech books, tech tutorials",
+};
+
+const AboutPage = () => {
+  return <div>About Page</div>;
+};
+
+export default AboutPage;
---
+++ book 2/book_store/app/githubusers/[user]/page.jsx
@@ -0,0 +1,5 @@
+const UserReposPage = ({ params: { user } }) => {
+  return <div>Users Repo Page for: {user}</div>;
+};
+
+export default UserReposPage;
